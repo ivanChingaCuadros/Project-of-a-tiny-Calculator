b7aa3df81091138b0d5cd587dd74fa68f011a69a
ivanChingaCuadros <ivan.chinga.cuadros1@gmail.com>
2022-01-03 23:59:58 +0100

    Funcionamiento js- html - css

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,10 +1,40 @@
-window.addEventListener('load', ()=>{
+window.addEventListener('load', ()=> {
     const display  = document.querySelector('.calculator-display');
     const keypadButtons = document.getElementsByClassName('keypad-button');
 
     const keypadButtonsArray = Array.from(keypadButtons);
 
-    keypadButtonsArray.forEach( (button)) => {
-        console.log(button)
+    keypadButtonsArray.forEach( (button) => {
+        button.addEventListener('click', ()=> {
+            calculadora(button, display);
+        })
     })
 });
+function calculadora(button, display){
+    switch (button.innerHTML) {
+        case 'C':
+            borrar(display);
+            break;
+
+        case '=':
+            calcular(display);
+            break;
+    
+        default:
+            acutalizar(display, button);
+            break;
+    }
+}
+function calcular(display) {
+    display.innerHTML = eval(display.innerHTML)
+}
+function acutalizar(display, button){
+    if (display.innerHTML == 0) {
+        display.innerHTML = '';
+    }
+    display.innerHTML +=  button.innerHTML; 
+    // Esto va ir poniendo cada numero que cliquemos al lado del otro dentro del display
+}
+function borrar(display){
+    display.innerHTML = 0;
+}
